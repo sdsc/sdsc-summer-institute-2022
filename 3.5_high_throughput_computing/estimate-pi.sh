91f81cccf0334420c80f1ee3b95701dd25dfc3ed
Marty Kandes <mkandes@sdsc.edu>
2022-07-29 09:33:47 -0700

    Create estimate-pi.sh

---
+++ 3.5_high_throughput_computing/estimate-pi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=estimate-pi
+#SBATCH --account=sds184
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:30:00
+#SBATCH --output=%x.o%j.%N
+
+module purge
+
+time -p python3 pi.py 100000
