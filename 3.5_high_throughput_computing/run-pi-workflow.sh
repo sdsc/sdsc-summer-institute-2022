d9bda6d22cc22d6eccf81ff5f017f87be67f7684
Marty Kandes <mkandes@sdsc.edu>
2022-07-30 14:32:58 -0700

    Create run-pi-workflow.sh

---
+++ 3.5_high_throughput_computing/run-pi-workflow.sh
@@ -0,0 +1,16 @@
+!/usr/bin/env bash
+
+#SBATCH --job-name=run-pi-workflow
+#SBATCH --account=sds184
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:30:00
+#SBATCH --output=%x.o%j.%N
+
+module reset
+
+job_id="$(sbatch estimate-pi.sh | grep -o '[[:digit:]]*')"
+sbatch "--dependency=afterok:${job_id}" compute-pi-stats.sh "${job_id}"
