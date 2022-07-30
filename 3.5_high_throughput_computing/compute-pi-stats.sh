4fcfc4bb4fb97e215ca85c89a53222dca884a85d
Marty Kandes <mkandes@sdsc.edu>
2022-07-30 13:52:23 -0700

    Create compute-pi-stats.sh

---
+++ 3.5_high_throughput_computing/compute-pi-stats.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=compute-pi-stats
+#SBATCH --account=sds184
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:30:00
+#SBATCH --output=%x.o%j.%N
+
+declare -xir ARRAY_JOB_ID="${1}"
+
+module reset
+module load gcc
+module load gnuplot
+
+echo "$(cat estimate-pi.o${ARRAY_JOB_ID}.*)" | \
+  gnuplot -e 'stats "-"; print STATS_mean, STATS_stddev'
