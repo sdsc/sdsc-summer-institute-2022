14a59f0d7f673baaa1017518ae6016b497b52dbb
Marty Kandes <mkandes@sdsc.edu>
2022-08-01 11:04:23 -0700

    Update run-ior-benchmark.sh

--- 2.5_data_management/run-ior-benchmark.sh
+++ 2.5_data_management/run-ior-benchmark.sh
@@ -4,6 +4,7 @@
 #SBATCH --account=crl155
 #SBATCH --reservation=SI2022DAY1
 #SBATCH --partition=compute
+#SBATCH --qos=normal-eot
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=1
